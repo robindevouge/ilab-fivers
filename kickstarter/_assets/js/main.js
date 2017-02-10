5516c7004163cdbe8c7c5c03065acd3356e0ef06
Banjopalmo <palmerobenjamin@gmail.com>
2017-02-10 10:12:52 +0100

    New header + content update

--- kickstarter/_assets/js/main.js
+++ kickstarter/_assets/js/main.js
@@ -1,10 +1,22 @@
+// If js is activated, hide images to make them appear later
+
+var images = document.getElementsByClassName("animated_img");
+
+for (i = 0; i < images.length; i++) {
+  images[i].classList.remove("appear-left");
+  images[i].classList.remove("appear-right");
+}
+
 var left0 = document.getElementsByClassName("img__left")[0];
 var right0 = document.getElementsByClassName("img__right")[0];
 var left1 = document.getElementsByClassName("img__left")[1];
 var right1 = document.getElementsByClassName("img__right")[1];
 var left2 = document.getElementsByClassName("img__left")[2];
 var right2 = document.getElementsByClassName("img__right")[2];
-
+var left3 = document.getElementsByClassName("img__left")[3];
+var right3 = document.getElementsByClassName("img__right")[3];
+var left4 = document.getElementsByClassName("img__left")[4];
+var right4 = document.getElementsByClassName("img__right")[4];
 
 if ( $(window).width() < 700) {      
     $(document).ready(function() {
@@ -16,7 +28,7 @@ if ( $(window).width() < 700) {
         gutterwidth = 0;
         pagetopmargin = 35;
         rowheight = 25.6;
-        gridonload = 'on';
+        gridonload = 'off';
         makehugrid();
         setgridonload();
     }); 
@@ -46,17 +58,33 @@ if ( $(window).width() < 700) {
       }
     })
 
-} else{
+    var waypoint_touch = new Waypoint({
+      element: document.getElementById('heat'),
+      handler: function() {
+        left3.classList.add("appear-left");
+        right3.classList.add("appear-right");
+      }
+    })
+
+    var waypoint_touch = new Waypoint({
+      element: document.getElementById('material'),
+      handler: function() {
+        left4.classList.add("appear-left");
+        right4.classList.add("appear-right");
+      }
+    })
+
+} else {
     $(document).ready(function() {
         pageUnits = '%';
         colUnits = '%';
         pagewidth = 90;
-        columns = 4;
-        columnwidth = 22;
-        gutterwidth = 4;
+        columns = 10;
+        columnwidth = 7.3;
+        gutterwidth = 3;
         pagetopmargin = 35;
         rowheight = 25.6;
-        gridonload = 'on';
+        gridonload = 'off';
         makehugrid();
         setgridonload();
     });
@@ -87,6 +115,22 @@ if ( $(window).width() < 700) {
       },
       offset: 300
     })
+
+    var waypoint_touch = new Waypoint({
+      element: document.getElementById('heat'),
+      handler: function() {
+        left3.classList.add("appear-left");
+        right3.classList.add("appear-right");
+      },
+      offset: 300
+    })
+
+    var waypoint_touch = new Waypoint({
+      element: document.getElementById('material'),
+      handler: function() {
+        left4.classList.add("appear-left");
+        right4.classList.add("appear-right");
+      },
+      offset: 300
+    })
 };
-
-
